553294aed0ba0fd1909d91f5096b35def4cfc655
NicolMejiaS <84528924+NicolMejiaSegura@users.noreply.github.com>
2021-11-12 14:00:00 -0600

    +Se carga la primera vista

---
+++ WebArdabyCC/assets/js/menu.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($("#menu").offset().top > 56) {
+            $("#menu").addClass("cambioMenu");
+        } else {
+            $("#menu").removeClass("cambioMenu");
+        }
+    })
+
+});
